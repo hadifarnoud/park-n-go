1e408fb55bd4e3e483ce9898eafbffbe45c99149
Hadi Farnoud <hadi.farnoud@gmail.com>
2012-12-21 16:25:31 +0000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ParkNGo::Application.config.secret_token = '09d38b075c5297d0f12fa01f6829f1e7b721f5a4c74059174a5b435e951caea4e0caabf5280556a72ebd1172921a610e8b6161d0d2b9d9fa83d03a9282609b73'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ParkNGo::Application.config.session_store :cookie_store, key: '_park-n-go_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ParkNGo::Application.config.session_store :active_record_store
