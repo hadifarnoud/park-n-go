fa8f15dae8e532053533e49e596ba2cb935f7ec6
Hadi Farnoud <hadi.farnoud@gmail.com>
2013-01-02 21:04:36 +0000

    token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-ParkNGo::Application.config.secret_token = '09d38b075c5297d0f12fa01f6829f1e7b721f5a4c74059174a5b435e951caea4e0caabf5280556a72ebd1172921a610e8b6161d0d2b9d9fa83d03a9282609b73'
+ParkNGo::Application.config.secret_token = '09d3uy578t67t76r76r75c5297d0f12fa01f6829f1e7b721f5a4c74059174a5b435e998t6uyfgkytdmgfbdtvr5280556a72ebd1172921a610e8b6161d0d2b9d9fa83d03a9282609b73'
