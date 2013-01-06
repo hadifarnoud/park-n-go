a5be6d900d32000878598dc3a46ec6ce3da0132a
Hadi Farnoud <hadi.farnoud@gmail.com>
2013-01-06 23:39:38 +0000

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130106184754) do
+ActiveRecord::Schema.define(:version => 20130103050611) do
 
   create_table "addresses", :force => true do |t|
     t.integer  "address_id"
@@ -85,9 +85,8 @@ ActiveRecord::Schema.define(:version => 20130106184754) do
     t.string   "website"
     t.string   "telephone"
     t.string   "description"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
-    t.string   "company_name"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
   end
 
   create_table "parking_parking_branches", :id => false, :force => true do |t|
