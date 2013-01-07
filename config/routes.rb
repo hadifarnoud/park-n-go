00be5e646bb87fc179c466d7d338195e2d5c7d60
Hadi Farnoud <hadi.farnoud@gmail.com>
2013-01-07 19:05:23 +0000

    custom route test

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 ParkNGo::Application.routes.draw do
-  get "parking_company/index"
+  # get "parking_company/index"
+  match '/company', to: 'parking_company#index'
 
-  get "parking_company/show"
+  # get "parking_company/show"
+  match '/company/show', to: 'parking_company#show'
 
   # get "parking_company/new"
+  match '/company/add', to: 'parking_company#new'
   
-  get "parking_company/update"
+  # get "parking_company/update"
+  match '/company/update', to: 'parking_company#update'
 
   get "parking_company/destroy"
 
@@ -22,11 +26,12 @@ ParkNGo::Application.routes.draw do
 
   authenticated :user do
     root :to => 'home#index'
-    #match '/company/add' to: 'parking_companies#new'
+    
   end
   root :to => "home#index"
+
   devise_for :users, :path_names => { :sign_up => "register" }, :path_names => { :sign_in => "login" }
   resources :users
   resources :parking_companies
-  resources :parking_branches
+  # resources :parking_branches
 end
