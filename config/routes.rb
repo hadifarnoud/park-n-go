f8ad5e5a7d1621f612fe50d2f5e1467b948e04e0
Hadi Farnoud <hadi.farnoud@gmail.com>
2013-01-07 14:48:36 +0000

    routes exaple

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 ParkNGo::Application.routes.draw do
   authenticated :user do
     root :to => 'home#index'
+    #match '/company/add' to: 'parking_companies#add'
   end
   root :to => "home#index"
   devise_for :users, :path_names => { :sign_up => "register" }, :path_names => { :sign_in => "login" }
