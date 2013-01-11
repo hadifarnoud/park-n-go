a53ff5280b73fda14dff4e7f470047ebafd67507
Hadi Farnoud <hadi.farnoud@gmail.com>
2013-01-11 23:18:06 +0000

    route fix again

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 ParkNGo::Application.routes.draw do
 
-  
+  # get "parking_companies/new"
+  match '/company/add', to: 'parking_companies#new', :as => "add_parking_company"
+
   # get "parking_companies/index" custom route all_companies_path 
   # match '/parking_companies' => redirect('/company')
   match '/company', to: 'parking_companies#index', :as => "all_companies" 
   match '/company/:company_name', to: 'parking_companies#index', :as => "company_by_name" 
 
-  # get "parking_companies/new"
-  match '/company/add', to: 'parking_companies#new', :as => "add_parking_company"
+
   
   # get "parking_companies/show" custom route show_company_path
   match '/company/show/:company_name', to: 'parking_companies#show', :as => "show_company"
