6b8d1939324519f110ce5e006d1f9163378cd587
Hadi Farnoud <hadi.farnoud@gmail.com>
2013-01-11 23:25:36 +0000

    route fix again

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,21 @@ ParkNGo::Application.routes.draw do
 
   # get "parking_companies/new"
   match '/company/add', to: 'parking_companies#new', :as => "add_parking_company"
-
+  # get "parking_branch/new"
+  match '/company/branch/add', to: 'parking_branches#new', :as => "add_parking_branch"
   # get "parking_companies/index" custom route all_companies_path 
-  # match '/parking_companies' => redirect('/company')
   match '/company', to: 'parking_companies#index', :as => "all_companies" 
+
+  # match '/parking_companies' => redirect('/company')
   match '/company/:company_name', to: 'parking_companies#index', :as => "company_by_name" 
 
+  get "parking_companies/destroy"
 
+  get "parking_branches/index"
+
+  get "parking_branches/update"
+
+  get "parking_branches/destroy"
   
   # get "parking_companies/show" custom route show_company_path
   match '/company/show/:company_name', to: 'parking_companies#show', :as => "show_company"
@@ -23,22 +31,17 @@ ParkNGo::Application.routes.draw do
   # get "edit_parking_company" => parking_companies#edit custom route edit_parking_company_path
   match '/company/edit/:id', to: 'parking_companies#edit', :as => "edit_parking_company"
 
-  get "parking_companies/destroy"
 
-  get "parking_branch/index"
 
   # get "parking_branch/show"
   match '/company/branch/:id', to: 'parking_branches#show', :as => "show_company_branch"
   
-  # get "parking_branch/new"
-  match '/company/branch/add', to: 'parking_branches#new', :as => "add_parking_branch"
+
 
   #pass the id to path edit_parking_branch_path(id)
   match '/company/branch/edit/:id', to: 'parking_branches#edit', :as => "edit_parking_branch"
   
-  get "parking_branch/update"
 
-  get "parking_branch/destroy"
 
 
   authenticated :user do
