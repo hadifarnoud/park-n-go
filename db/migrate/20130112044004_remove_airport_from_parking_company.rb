7eb1621eb2ab644856be0a3ef1aab723e04fe2b3
Hadi Farnoud <hadi.farnoud@gmail.com>
2013-01-14 04:01:48 +0000

    heroku db migration failed on this one. removing

--- db/migrate/20130112044004_remove_airport_from_parking_company.rb
+++
@@ -1,9 +0,0 @@
-class RemoveAirportFromParkingCompany < ActiveRecord::Migration
-  def up
-  	remove_column :parking_companies, :airport
-  end
-
-  def down
-  	add_column :parking_companies, :airport, :string
-  end
-end
