2808a4932d79752fef36bc3b3272f56e35c18359
Hadi Farnoud <hadi.farnoud@gmail.com>
2013-01-28 01:07:18 +0000

    migration done

--- app/models/price.rb
+++ app/models/price.rb
@@ -1,5 +1,5 @@
 class Price < ActiveRecord::Base
-  attr_accessible :price_amount, :price_currency, :price_id
-  has_many :parkings
+  attr_accessible :price_amount, :price_currency
+  belongs_to :parkings
   has_many :services
 end
--- db/migrate/20130127222604_add_column_to_transaction.rb
+++ db/migrate/20130127222604_add_column_to_transaction.rb
@@ -1,4 +1,15 @@
 class AddColumnToTransaction < ActiveRecord::Migration
-  def change
+  def up
+  	addcolumn :prices, :base_price, :float
+  	addcolumn :prices, :duration, :float
+  	addcolumn :prices, :price_rule, :string
+  	removecolumn :prices, :price_id
+  end
+
+  def down
+  	addcolumn :prices, :price_id, :integer
+  	removecolumn :prices, :base_price
+  	removecolumn :prices, :duration
+  	removecolumn :prices, :price_rule
   end
 end
